8fae548224f5b5c2b86db3bfdc0be57380fc3ab5
GoncharenkoMA <snap56@mail.ru>
2022-08-24 15:56:46 +0300

    Изучение If and Else

---
+++ Example_If_Else/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите Имя пользователя: ");
+
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
+
+
+
+  
