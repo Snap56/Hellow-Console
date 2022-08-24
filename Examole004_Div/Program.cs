dc7f536f60c0bdbe2e2f80092ab6352bbd3085de
GoncharenkoMA <snap56@mail.ru>
2022-08-24 14:59:03 +0300

    Редактирование 4 примера.Добавили Рандом

--- Examole004_Div/Program.cs
+++ Examole004_Div/Program.cs
@@ -1,4 +1,13 @@
 ﻿Console.Write("Введите Первое число ");
-double numberA = Convert.ToDouble(Console.ReadLine());
-double NumberB = 5 ;
+
+int numberA = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine(numberA);
+
+int NumberB = new Random().Next(1,10) ;
+
+Console.WriteLine(NumberB);
+
+Console.Write("Делим первое число на второе. Результатравен ");
+
 Console.WriteLine(numberA / NumberB);
